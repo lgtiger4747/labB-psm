0f031a618b0444e6edba9eafd9ea2084069823b2
as7haro7 <erickbreack72@gmail.com>
2024-03-10 13:06:51 -0400

    envio de datos entre views

--- app/src/main/java/com/blab0/FacturaActivity.java
+++ app/src/main/java/com/blab0/FacturaActivity.java
@@ -9,11 +9,10 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class FacturaActivity extends AppCompatActivity {
-    TextView txtNom,txtCi,txtCons;
-    Boolean pl1,pl2,pl3,pl4,pl5;
-    String nom1="",ci1="";
-    int Opc[];
-
+    TextView txtNom,txtCi,txtCons,txtTipoPago;
+    String nom1="",ci1="",tipoPago="";
+    String[] plato = new String[10];
+    int[] precio = new int[10];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +21,21 @@ public class FacturaActivity extends AppCompatActivity {
         txtNom = (TextView)findViewById(R.id.textView3);
         txtCi = (TextView)findViewById(R.id.textView4);
         txtCons = (TextView)findViewById(R.id.tv_plato2);
-
+        txtTipoPago = (TextView)findViewById(R.id.textView14);
 
         Intent intent = getIntent();
         nom1 = intent.getStringExtra("nom");
         ci1= intent.getStringExtra("ci");
-        Opc = intent.getIntArrayExtra("plato1");
+        plato=intent.getStringArrayExtra("ListaPlatos");
+        precio=intent.getIntArrayExtra("ListaPrecios");
+        tipoPago=intent.getStringExtra("tipoPag");
 
         txtNom.setText(nom1);
         txtCi.setText(ci1);
+        txtCons.setText(plato[0]+": "+precio[0]+" Bs.");
+        txtTipoPago.setText(tipoPago);
+        System.out.println(plato[0]);
 
-        txtCons.setText(""+Opc[0]);
+
     }
 }
--- app/src/main/java/com/blab0/MainActivity.java
+++ app/src/main/java/com/blab0/MainActivity.java
@@ -6,44 +6,59 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtNombre,txtCi;
     CheckBox cbOpc1,cbOpc2,cbOpc3,cbOpc4,cbOpc5;
-
+    RadioGroup rgPago;
+    RadioButton rbContado,rbTargeta;
     String nombre="hola", ci="Hola2";
-    Boolean pla1,pla2,pla3,pla4,pla5;
-    int platos[];
+    String[] platos = new String[10];
+    int[] precios = new int[10];
 
-
+    private String tipoPago = "Tipo de Pago";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtNombre = (TextView)findViewById(R.id.edt_nombre);
         txtCi = (TextView)findViewById(R.id.edt_ci);
+
         cbOpc1 = (CheckBox) findViewById(R.id.ch_plato1);
         cbOpc2 = (CheckBox) findViewById(R.id.ch_plato2);
         cbOpc3 = (CheckBox) findViewById(R.id.ch_plato3);
         cbOpc4 = (CheckBox) findViewById(R.id.ch_refresco1);
         cbOpc5 = (CheckBox) findViewById(R.id.ch_refresco2);
 
+        rgPago=(RadioGroup) findViewById(R.id.forma_pago);
+        rbContado=(RadioButton) findViewById(R.id.rb_contado);
+        rbTargeta=(RadioButton) findViewById(R.id.rb_tar);
 
-        cbOpc1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-
-                }else{
-
-                }
+        cbOpc1.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                platos[0]="pique macho";
+                precios[0]=30;
+            } else {
+                platos[0]="no";
+                precios[0]=0;
             }
         });
+
+        //Ver estado del RadioGroup para ver el tipo de pago
+        rgPago.setOnCheckedChangeListener((group, checkedId) -> {
+            if (rbContado.isChecked()){
+                tipoPago="contado";
+            }else if (rbTargeta.isChecked()){
+                tipoPago="targeta";
+            }
+        });
+
     }
     public void cambioLayout(View view){
         nombre = txtNombre.getText().toString();
@@ -52,7 +67,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, FacturaActivity.class);
         intent.putExtra("nom",nombre);
         intent.putExtra("ci",ci);
-        intent.putExtra("plato1",platos);
+        intent.putExtra("ListaPlatos", platos);
+        intent.putExtra("ListaPrecios", precios);
+        intent.putExtra("tipoPag", tipoPago);
         startActivity(intent);
     }
 }
